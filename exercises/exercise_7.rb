62ff5cb6a0024b06576fc9140fe93678574274ba
Ann <a.m.hnatyshyn@gmail.com>
2024-09-26 15:07:17 -0300

    complete

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,18 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+
+class Employee < ApplicationRecord
+  validates :first_name, :last_name, presence: true
+  validates :hourly_rate, only_integer: true, length: {:in 40..200}
+  validates :employees, :store, presence: true
+  validates :store_name, presence: true, length: { minimum: 3 }
+  validates :store, annual_revenue: true, only_integer: true, length: { minimum: 0 }
+
+  def mens_womens_apparel
+    unless mens_apparel == true || womens_apparel == true
+      errors.add(:base "Stores must carry either men's or women's apparel")
+    end
+
+end
+
